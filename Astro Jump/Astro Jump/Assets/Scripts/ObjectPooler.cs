e24b0a4854f92f478f8a20b1779b2c4934ec0e4e
Mayank Bagchi <mbagchi17@gmail.com>
2018-04-13 21:51:17 +0530

    Changed Spawning and Pppoling Script.
    Fixed the issue of infinite creation of platforms even when object pooling was used. But still need to fix the null exception it throws.

---
+++ Astro Jump/Astro Jump/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour {
+
+	public static ObjectPooler instance;
+
+	void Awake()
+	{
+		instance = this;
+	}
+	public GameObject prefab,platforms;
+	public int size;
+//	public List<GameObject> plat;
+	public Queue<GameObject> plat;
+//	bool willGrow=true;
+
+//	public GameObject op;
+	public float xPos,yPos=0.1f;
+	public float xRange=2;
+	public float yMin=0.01f;
+	public float yMax= 1;
+
+	public void Start () {
+		plat = new Queue<GameObject> ();
+		for (int i = 0; i < size; i++) {
+			GameObject o = (GameObject)Instantiate(prefab);
+			o.SetActive (false);
+			plat.Enqueue(o);
+		}
+	}
+
+	void FixedUpdate () {
+//		xPos = Random.Range (-xRange, xRange);
+//		GameObject
+//		platforms=GetPlatform ();
+		GetPlatform();
+		platforms.transform.position = new Vector2 (0, yPos);
+		platforms.SetActive (true);
+//		yPos += Random.Range (yMin, yMax);
+	}
+
+
+	public void GetPlatform() {
+//		print (plat.Count);
+//		int i=0;
+//		for (i = 0; i < plat.Count; i++) {
+//			if (!plat [i].activeInHierarchy) {
+//				print ("returning plat");
+//				return plat [i];
+//			}
+//		}
+//
+//		if (willGrow) {
+//			print ("Growing");
+//			GameObject o = (GameObject)Instantiate (prefab);
+//			plat.Add (o);
+//			return o;
+//		}
+//		return null;
+//		if (plat.Count<1) 
+//		{
+//			GameObject o = (GameObject)Instantiate(prefab);
+//			o.SetActive (false);
+//			plat.Enqueue(o);
+//			print("Low Count"
+//		}
+		try {
+			platforms=plat.Dequeue ();
+			yPos += Random.Range (yMin, yMax);
+		} catch (System.Exception ex) {
+			Debug.LogException (ex,this);
+		}
+
+	}
+
+	public void DeletePlatform(GameObject go)
+	{
+		go.SetActive (false);
+		plat.Enqueue (go);
+	}
+}
--- Astro Jump/Astro Jump/Assets/Scripts/Platform.cs
+++ Astro Jump/Astro Jump/Assets/Scripts/Platform.cs
@@ -3,29 +3,22 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour {
 
-	public float jumpForce;
-	public bool invisible=false;
-
-	public static Platform instance;
-	void OnAwake()
+	public float jumpForce,someVal;
+	void Update()
 	{
-		instance=this;
-	}
-
-	void OnBecameInvisible()
-	{
-		invisible = true;
+		if (gameObject.transform.position.y < Camera.main.transform.position.y - someVal){
+			ObjectPooler.instance.DeletePlatform (gameObject);
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D col)
 	{
-		if (col.relativeVelocity.y <= 0)
-		{
-			Rigidbody2D rb = col.collider.GetComponent<Rigidbody2D>();
-			if (rb != null)
-			{
-				rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+		if (col.relativeVelocity.y <= 0) {
+			Rigidbody2D rb = col.collider.GetComponent<Rigidbody2D> ();
+			if (rb != null) {
+				rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
 			}
 		}
 	}
+
 }
