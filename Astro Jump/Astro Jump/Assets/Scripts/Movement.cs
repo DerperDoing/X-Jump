7a353536b528f44c3677b762ed40b035440e4039
Mayank Bagchi <mbagchi17@gmail.com>
2018-04-30 00:45:28 +0530

    Added Screen Wrapping
    Added the screen wrapping functionality. When the character goes out of screen from the left, it re-appears on the right side of the screen from that exact y-position and vice-versa.

--- Astro Jump/Astro Jump/Assets/Scripts/Movement.cs
+++ Astro Jump/Astro Jump/Assets/Scripts/Movement.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 	public float speed=10f;
+	float leftConstraint, rightConstraint;
+	public float buffer;
+
+	void Awake()
+	{
+		leftConstraint = Camera.main.ScreenToWorldPoint (new Vector2 (0.0f, 0.0f)).x;
+		rightConstraint = Camera.main.ScreenToWorldPoint (new Vector2 (Screen.width, 0.0f)).x;
+	}
 	void FixedUpdate () {
 		float x = Input.acceleration.x;
 		transform.Translate (x, 0f, 0f);
+
+		if (this.transform.position.x < leftConstraint - buffer) {
+			this.transform.position = new Vector2 (rightConstraint + buffer, this.transform.position.y);
+		} else if (this.transform.position.x > rightConstraint + buffer) {
+			this.transform.position = new Vector2 (leftConstraint - buffer, this.transform.position.y);
+		}
 	}
 }
